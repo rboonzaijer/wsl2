d0eda3a2725212c63d48f72617a8f7d459905981
Roel <10514742+rboonzaijer@users.noreply.github.com>
2023-01-28 16:44:04 +0100

    newlines

--- install-php8.0.sh
+++ install-php8.0.sh
@@ -1,5 +1,33 @@
 # Note: run ./install-php-repo.sh first! (once)
 
 sudo apt-get install -y php8.0
-sudo apt-get install -y php8.0-bcmath php8.0-bz2 php8.0-cli php8.0-common php8.0-curl php8.0-fpm php8.0-gd php8.0-http php8.0-imagick php8.0-imap php8.0-intl php8.0-mbstring php8.0-mcrypt php8.0-mongodb php8.0-mysql php8.0-odbc php8.0-opcache php8.0-pgsql php8.0-redis php8.0-soap php8.0-sqlite3 php8.0-ssh2 php8.0-uuid php8.0-xdebug php8.0-xml php8.0-yaml php8.0-zip
+
+sudo apt-get install -y
+    php8.0-bcmath \
+    php8.0-bz2 \
+    php8.0-cli \
+    php8.0-common \
+    php8.0-curl \
+    php8.0-fpm \
+    php8.0-gd \
+    php8.0-imagick \
+    php8.0-imap \
+    php8.0-intl \
+    php8.0-mbstring \
+    php8.0-mcrypt \
+    php8.0-mongodb \
+    php8.0-mysql \
+    php8.0-odbc \
+    php8.0-opcache \
+    php8.0-pgsql \
+    php8.0-redis \
+    php8.0-soap \
+    php8.0-sqlite3 \
+    php8.0-ssh2 \
+    php8.0-uuid \
+    php8.0-xdebug \
+    php8.0-xml \
+    php8.0-yaml \
+    php8.0-zip
+
 php8.0 -v

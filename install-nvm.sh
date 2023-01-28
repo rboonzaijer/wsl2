df71efb67b2d97171fc95ed860f14cd5b1a0013f
Roel <10514742+rboonzaijer@users.noreply.github.com>
2023-01-28 16:44:56 +0100

    make sure bash-refresh is applied

--- install-nvm.sh
+++ install-nvm.sh
@@ -7,7 +7,6 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 nvm install lts/*
 nvm use lts/*
 
-# https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
 source ~/.bashrc
 
 npm install -g yarn

1895f403ac09c9b6d3d6f2222d6aba047cd5a8a3
Roel <10514742+rboonzaijer@users.noreply.github.com>
2023-11-01 23:08:47 +0100

    update

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -1,6 +1,6 @@
 # https://github.com/nvm-sh/nvm#install--update-script
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 

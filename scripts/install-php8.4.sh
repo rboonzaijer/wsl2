2fee83c7f17ebdf2e34710003857e3c96e4ee413
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-01-10 16:10:38 +0100

    php 8.4

---
+++ scripts/install-php8.4.sh
@@ -0,0 +1,32 @@
+# Note: run ./install-php-repo.sh first! (once)
+
+sudo apt-get install -y php8.4
+
+sudo apt-get install -y php8.4-bcmath \
+    php8.4-bz2 \
+    php8.4-cli \
+    php8.4-common \
+    php8.4-curl \
+    php8.4-fpm \
+    php8.4-gd \
+    php8.4-imagick \
+    php8.4-imap \
+    php8.4-intl \
+    php8.4-mbstring \
+    php8.4-mcrypt \
+    php8.4-mongodb \
+    php8.4-mysql \
+    php8.4-odbc \
+    php8.4-opcache \
+    php8.4-pgsql \
+    php8.4-redis \
+    php8.4-soap \
+    php8.4-sqlite3 \
+    php8.4-ssh2 \
+    php8.4-uuid \
+    php8.4-xdebug \
+    php8.4-xml \
+    php8.4-yaml \
+    php8.4-zip
+
+php8.4 -v

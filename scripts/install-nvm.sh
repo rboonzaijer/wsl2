a45100d30fbfc11b339d1a22170cc2dc36773e82
Roel <10514742+rboonzaijer@users.noreply.github.com>
2025-09-20 22:24:32 +0200

    use corepack enable for yarn

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -1,6 +1,7 @@
 # https://github.com/nvm-sh/nvm#install--update-script
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
@@ -9,7 +10,8 @@ nvm use lts/*
 
 source ~/.bashrc
 
-npm install -g yarn
+#npm install -g yarn
+corepack enable
 
 echo 'node'
 node -v
